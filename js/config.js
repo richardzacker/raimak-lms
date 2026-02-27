903e0489b5d6b378d46eed98cff971322080432e
Richard Zacker <richardzacker@gmail.com>
2026-02-27 13:35:46 -0600

    Fix formatting in config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const Config = {
 
   azure: {
     clientId:    "ad1b153f-8b6a-4f1c-8ab2-58fcf03cf5c2",
-    tenantId:    "39e14190-0b23-4ecd-99f9-606ad1215881"
+    tenantId:    "39e14190-0b23-4ecd-99f9-606ad1215881",
     redirectUri: window.location.origin + window.location.pathname,
   },
 
