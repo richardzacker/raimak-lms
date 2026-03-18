133aa000c20244ae5826b063768c7c5f8ff77b50
Richard Zacker <richardzacker@gmail.com>
2026-03-18 11:22:47 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@
 const Config = {
 
   azure: {
-    clientId:    "YOUR_AZURE_APP_CLIENT_ID",
-    tenantId:    "YOUR_TENANT_ID",
+    clientId:    "ad1b153f-8b6a-4f1c-8ab2-58fcf03cf5c2",
+    tenantId:    "39e14190-0b23-4ecd-99f9-606ad1215881",
     redirectUri: window.location.origin + window.location.pathname,
   },
 
@@ -78,6 +78,8 @@ const Config = {
       // Add admin emails here — remove the // to activate
       // "RichardZacker@raimak.com",
       // "B.Hinesley@raimak.com",
+      // "S.Balleste@raimak.com",
+      // "antoinette.bickel@raimak.com",
     ],
   },
 
